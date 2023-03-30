8cf77de5c3baef28de37ad559585be0a1bdcbf6a
davidgoulart-dev <davidgoulart.tae@gmail.com>
2023-03-30 18:35:22 -0300

    Página home estilizada, só faltando o botão subscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-
+import styles from './home.module.scss';
 import { Inter } from 'next/font/google'
 import Head from 'next/head'
 
@@ -11,7 +11,21 @@ export default function Home() {
       <Head>
         <title>Home | ig.news</title>
       </Head>
-      <h1 >Hello Word</h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+        Get access to all the publications <br />
+        <span>for $9.90 per month</span>
+      </p>
+
+        </section>
+
+        <img src='/images/avatar.svg' alt='Girl Coding'/> 
+
+      </main>
     </>
   )
 }
