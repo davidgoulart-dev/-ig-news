47e5e8f426aa0d4df1dbf75654e64484aa9810b1
David Goulart <davidgoulart.tae@gmail.com>
2023-04-03 19:43:24 +0000

    Correção no código do servidor do prismic

--- src/services/prismic.ts
+++ src/services/prismic.ts
@@ -1,7 +1,7 @@
 import Prismic from '@prismicio/client'
 
 export function getPrismicClient( req?: unknown ) {
-    const prismic = Prismic.client(
+    const prismic = new Prismic.Client(
         process.env.PRISMIC_ENDPOINT as string,
         {
             req,
